e84edb178b58eaf4a7db4c91f8c03a14d9ad610d
Teemu K <teemu.kaunisto@gmail.com>
2021-01-14 20:58:16 +0200

    Add sort queryFunction business logic

--- src/extensions/queryFunctions.ts
+++ src/extensions/queryFunctions.ts
@@ -1,33 +1,54 @@
 'use strict'
 import Hapi from '@hapi/hapi';
 import Boom from '@hapi/boom'
+import assert from 'assert';
 
 const sort = (objArr :object[], queryString :string) => {
 
+    const regex = /^\('(.+?)','((asc)|(desc))'\)$/
+
+    assert(regex.test(queryString), Boom.notImplemented('Check function syntax'))
+
+    const [,property, order] = regex.exec(queryString)!
+
+    const sorted = [...objArr].sort((a :any,b :any) => {
+        
+        const valueA = typeof a[property] === "string" ? a[property].toLowerCase() : a[property]
+        const valueB = typeof b[property] === "string" ? b[property].toLowerCase() : b[property]
+
+        if (valueA > valueB) {
+            return order === 'asc' ? 1 : -1
+        } else if (valueA < valueB) {
+            return order === 'asc' ? -1 : 1
+        } else {
+            return 0
+        }
+
+    })
 
 
-    return objArr
+    return sorted
 }
 
 
 const queryFunctions = (request :Hapi.Request, h :Hapi.ResponseToolkit) => {
 
     const query = request.query
+    const functions = Object.keys(query)
 
-    if (query) {
+    if (functions.length) {
 
-        const functions = Object.keys(query)
         const source = (h.request.response as Hapi.ResponseObject).source
 
         if (Array.isArray(source)) {
 
-            let data = [...source]
+            let data: object[] = []
 
             for (const funcName of functions) {
 
                 switch (funcName) {
                     case 'sort':
-                        data = sort(data, query[funcName])
+                        data = sort(source, query[funcName])
                         break;
                 
                     default:
